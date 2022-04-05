87ef3217a65ce54727ca90c79ccb107eb18c6f40
Cindy Chiu <yc506@duke.edu>
2022-04-04 23:45:55 -0400

    revise eda plots accordingly

--- 10_code/plot_eda.py
+++ 10_code/plot_eda.py
@@ -3,42 +3,50 @@ import seaborn as sns
 import pandas as pd
 import altair as alt
 
-def plot_monthly_total_rate_trend(df: pd.DataFrame, col_type: str):
+
+def plot_monthly_total_rate_trend(df: pd.DataFrame, col_type):
     """
     Input: a data frame contained monthly total crime rate
     Input col_type : male, female, black, asian, amer_ind, white, arrests
     ouput: a graph that splits 2019 and 2020 by county
     """
     # convert month from string to categorial in order to sort them
-    cat_type = CategoricalDtype(
-        categories=[
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December",
-        ],
-        ordered=True,
+
+    df["month"] = df["month"].cat.rename_categories(
+        {
+            1: "Jan",
+            2: "Feb",
+            3: "Mar",
+            4: "Apr",
+            5: "May",
+            6: "Jun",
+            7: "Jul",
+            8: "Aug",
+            9: "Sep",
+            10: "Oct",
+            11: "Nov",
+            12: "Dec",
+        }
     )
 
-    df["month"] = df["month"].astype(cat_type)
     g = sns.FacetGrid(df, col="year", hue="fips_state_county_code")
     sub_str = "arrest_rate_gt_" + col_type
     g.map(sns.lineplot, "month", sub_str)
     g.set_xticklabels(rotation=45)
     g.fig.set_figwidth(10)
     g.fig.set_figheight(6)
-    g.fig.suptitle("Total Crime Rate Trends of " + col_type + " by County")
+    g.fig.suptitle("Total Arrest Rate Trends of " + col_type + " by County")
     g.fig.legend(
         title="County_name",
-        labels=["Adams", "Boulder", "Broomfield", "Denver", "El Paso"],
+        labels=[
+            "Adams",
+            "Arapahoe",
+            "Boulder",
+            "Broomfield",
+            "Denver",
+            "Douglas",
+            "Jefferson",
+        ],
         loc="upper right",
     )
 
@@ -51,25 +59,24 @@ def plot_monthly_certain_arrest_rate(df: pd.DataFrame, col_type: str, crime_type
     Input col_type : male, female, black, asian, amer_ind, white, hispanic, non_hispanic
     ouput: a graph that splits 2019 and 2020 by county
     """
-    cat_type = CategoricalDtype(
-        categories=[
-            "january",
-            "february",
-            "march",
-            "april",
-            "may",
-            "june",
-            "july",
-            "august",
-            "september",
-            "october",
-            "november",
-            "december",
-        ],
-        ordered=True,
+
+    df["month"] = df["month"].cat.rename_categories(
+        {
+            1: "Jan",
+            2: "Feb",
+            3: "Mar",
+            4: "Apr",
+            5: "May",
+            6: "Jun",
+            7: "Jul",
+            8: "Aug",
+            9: "Sep",
+            10: "Oct",
+            11: "Nov",
+            12: "Dec",
+        }
     )
 
-    df["month"] = df["month"].astype(cat_type)
     # we only calculate the total arrest rate in the original function
     # now we're going to plot the rate so we need to calculate it here
     sub_str = crime_type + "_tot_" + col_type
@@ -83,13 +90,20 @@ def plot_monthly_certain_arrest_rate(df: pd.DataFrame, col_type: str, crime_type
     # change the legend to more descriptive county names
     g.fig.legend(
         title="County_name",
-        labels=["Adams", "Boulder", "Broomfield", "Denver", "El Paso"],
+        labels=[
+            "Adams",
+            "Arapahoe",
+            "Boulder",
+            "Broomfield",
+            "Denver",
+            "Douglas",
+            "Jefferson",
+        ],
         loc="upper right",
     )
 
 
 def plot_grouped_arrest_trend(df):
-
 
     grouped_means = df.groupby(["treatment", "date"], as_index=False)[
         ["arrest_rate_gt_arrests"]
@@ -103,9 +117,8 @@ def plot_grouped_arrest_trend(df):
             color="treatment:N",
         )
     )
-    return scatter.properties(
-        title='Mean Total Arrest Rate by Time'
-    )
+    return scatter.properties(title="Mean Total Arrest Rate by Time")
+
 
 def main():
     df_total_arrest = pd.read_csv("./20_intermediate_files/aggregated.csv")
@@ -116,7 +129,7 @@ def main():
     plot_monthly_total_rate_trend(df_total_arrest, "white")
     plot_monthly_total_rate_trend(df_total_arrest, "asian")
     plot_monthly_total_rate_trend(df_total_arrest, "amer_ind")
-    plot_grouped_arrest_trend(df_total_arrest )
+    plot_grouped_arrest_trend(df_total_arrest)
 
 
 if __name__ == main:
